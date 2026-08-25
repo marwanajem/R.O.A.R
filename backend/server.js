9756154c7fd50cf0505b13502098cc75ec755c8e
Marwa Najem <marwanajem16@gmail.com>
2026-08-25 20:31:23 +0800

    Fixed nested listen error

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.get('/api/status', (req, res) => {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
- app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log(`Server running on port ${PORT}`);
 });
 
 
